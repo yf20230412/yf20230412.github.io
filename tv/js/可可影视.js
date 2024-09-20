442668b4ce41d43bd863bff24e4984304b7b3abb
yf1688 <yufeng1688778@163.com>
2024-09-20 09:18:52 +0800

    日常更新

--- tv/js/可可影视.js
+++ tv/js/可可影视.js
@@ -1,7 +1,7 @@
 var rule = {
     title: '可可影视[优]',
-    host: 'https://www.kkys01.com',
-    //host: 'https://keke5.app',
+    // host: 'https://www.kkys02.com',
+    host: 'https://www.keke1.app',
     // url: '/show/fyclass-----2-fypage.html',
     url: '/show/fyclass-fyfilter-fypage.html',
     filter_url: '{{fl.类型}}-{{fl.地区}}-{{fl.语言}}-{{fl.年份}}-{{fl.排序}}',
@@ -12,24 +12,22 @@ var rule = {
     headers: {
         'User-Agent': 'MOBILE_UA',
     },
-    //class_parse: '#nav-swiper&&.nav-swiper-slide;a&&Text;a&&href;/(\\w+).html',
-    //cate_exclude: 'Netflix|今日更新|专题列表|排行榜',
-    class_name:'电视剧&电影&综艺记录&动漫&短剧',
-    class_url:'2&1&4&3&6',    
+    class_parse: '#nav-swiper&&.nav-swiper-slide;a&&Text;a&&href;/(\\w+).html',
+    cate_exclude: 'Netflix|今日更新|专题列表|排行榜',
     tab_order: ['超清', '蓝光', '极速蓝光'],
     tab_rename:{'超清':'🌺风言锋语88🌺超清','4K(高峰不卡)':'🌺风言锋语88🌺4K'},
     play_parse: true,
     lazy: '',
-    limit: 6,
+    limit: 20,
     推荐: '.section-box:eq(2)&&.module-box-inner&&.module-item;*;*;*;*',
     double: false,
-    一级: '.module-box-inner&&.module-item;.v-item-title:eq(-1)&&Text;img&&data-original;.v-item-bottom&&span&&Text;a&&href',
+    一级: '.module-box-inner&&.module-item;.v-item-title:eq(1)&&Text;img:eq(1)&&data-original;.v-item-bottom&&span:eq(1)&&Text;a&&href',
     二级: {
-        title: '.detail-title&&Text;.detail-tags&&a:eq(-2)&&Text',
-        img: '.detail-pic&&img&&data-original',
+        title: '.detail-title&&strong:eq(1)&&Text;.detail-tags&&a:eq(-2)&&Text',
+        img: '.detail-pic&&img:eq(1)&&data-original',
         desc: '.detail-info-row-main:eq(-2)&&Text;.detail-tags&&a&&Text;.detail-tags&&a:eq(1)&&Text;.detail-info-row-main:eq(1)&&Text;.detail-info-row-main&&Text',
         content: '.detail-desc&&Text',
-        tabs: '.source-item-label',
+        tabs: 'body&&#detail-source-swiper&&span:not(:matches(可可影视提供))',
         lists: '.episode-list:eq(#id) a',
     },
     搜索: '.search-result-list&&a;.title&&Text;*;.search-result-item-header&&Text;a&&href;.desc&&Text',
@@ -37,7 +35,7 @@ var rule = {
     //     log(input);
     //    input = input.replace(rule.host,'https://vres.a357899.cn');
     // }),
-    //图片替换: 'https://keke5.app=>https://vres.a357899.cn',
+    //图片替换: 'https://www.keke5.app=>https://vres.a357899.cn',
     预处理: $js.toString(() => {
         let html = request(rule.host);
         let scripts = pdfa(html, 'script');
@@ -51,316 +49,14 @@ var rule = {
             rule.图片替换 = rule.host + '=>' + img_host;
         }
     }),
-    filter: {"1":[		
-	{"key":"class","name":"剧情","value":[
-    {"n":"全部剧情","v":""},
-    {"n":"Netflix","v":"Netflix"},		
-    {"n":"剧情","v":"剧情"},		
-    {"n":"喜剧","v":"喜剧"},		
-    {"n":"动作","v":"动作"},
-    {"n":"爱情","v":"爱情"},
-    {"n":"恐怖","v":"恐怖"},
-    {"n":"惊悚","v":"惊悚"},
-    {"n":"犯罪","v":"犯罪"},
-    {"n":"科幻","v":"科幻"},
-    {"n":"悬疑","v":"悬疑"},
-    {"n":"奇幻","v":"奇幻"},
-    {"n":"冒险","v":"冒险"},
-    {"n":"战争","v":"战争"},
-    {"n":"历史","v":"历史"},
-    {"n":"古装","v":"古装"},
-    {"n":"家庭","v":"家庭"},
-    {"n":"传记","v":"传记"},
-    {"n":"武侠","v":"武侠"},
-    {"n":"歌舞","v":"歌舞"},
-    {"n":"短片","v":"短片"},
-    {"n":"动画","v":"动画"},
-    {"n":"儿童","v":"儿童"},
-    {"n":"职场","v":"职场"}
-    ]
-    },
-	{"key":"area","name":"地区","value":[
-    {"n":"全部地区","v":""},
-    {"n":"大陆","v":"大陆"},
-    {"n":"香港","v":"香港"},
-    {"n":"台湾","v":"台湾"},
-    {"n":"美国","v":"美国"},
-    {"n":"日本","v":"日本"},
-    {"n":"韩国","v":"韩国"},
-    {"n":"英国","v":"英国"},
-    {"n":"法国","v":"法国"},
-    {"n":"德国","v":"德国"},
-    {"n":"印度","v":"印度"},
-    {"n":"泰国","v":"泰国"},
-    {"n":"丹麦","v":"丹麦"},
-    {"n":"瑞典","v":"瑞典"},
-    {"n":"巴西","v":"巴西"},
-    {"n":"加拿大","v":"加拿大"},
-    {"n":"俄罗斯","v":"俄罗斯"},
-    {"n":"意大利","v":"意大利"},
-    {"n":"比利时","v":"比利时"},
-    {"n":"爱尔兰","v":"爱尔兰"},
-    {"n":"西班牙","v":"西班牙"},
-    {"n":"澳大利亚","v":"澳大利亚"},
-    {"n":"其他","v":"其他"}
-    ]
-    },		
-    {"key":"year","name":"时间","value":[
-    {"n":"全部时间","v":""},
-    {"n":"2024","v":"2024"},		
-    {"n":"2023","v":"2023"},
-    {"n":"2022","v":"2022"},
-    {"n":"2021","v":"2021"},
-    {"n":"2020","v":"2020"},
-    {"n":"10年代","v":"10年代"},
-    {"n":"00年代","v":"00年代"},
-    {"n":"90年代","v":"90年代"},
-    {"n":"80年代","v":"80年代"},
-    {"n":"更早","v":"更早"}
-    ]
-    },
-    {"key":"by","name":"排序","value":[
-    {"n":"全部排序","v":""},
-    {"n":"时间排序","v":"time"},
-    {"n":"人气排序","v":"hits"},
-    {"n":"评分排序","v":"score"}
-    ]
-    }
-    ],
-
-    "2":[	
-    {"key":"class","name":"剧情","value":[
-    {"n":"全部剧情","v":""},
-    {"n":"Netflix","v":"Netflix"},
-    {"n":"剧情","v":"剧情"},
-    {"n":"爱情","v":"爱情"},
-    {"n":"喜剧","v":"喜剧"},
-    {"n":"犯罪","v":"犯罪"},
-    {"n":"悬疑","v":"悬疑"},
-    {"n":"古装","v":"古装"},
-    {"n":"动作","v":"动作"},
-    {"n":"家庭","v":"家庭"},
-    {"n":"惊悚","v":"惊悚"},
-    {"n":"奇幻","v":"奇幻"},
-    {"n":"美剧","v":"美剧"},
-    {"n":"科幻","v":"科幻"},
-    {"n":"历史","v":"历史"},
-    {"n":"战争","v":"战争"},
-    {"n":"韩剧","v":"韩剧"},
-    {"n":"武侠","v":"武侠"},
-    {"n":"言情","v":"言情"},
-    {"n":"恐怖","v":"恐怖"},
-    {"n":"冒险","v":"冒险"},
-    {"n":"都市","v":"都市"},
-    {"n":"职场","v":"职场"}
-    ]
-    },
-    {"key":"area","name":"地区","value":[
-    {"n":"全部地区","v":""},
-    {"n":"大陆","v":"大陆"},
-    {"n":"香港","v":"香港"},
-    {"n":"韩国","v":"韩国"},
-    {"n":"美国","v":"美国"},
-    {"n":"日本","v":"日本"},
-    {"n":"法国","v":"法国"},
-    {"n":"英国","v":"英国"},
-    {"n":"德国","v":"德国"},
-    {"n":"台湾","v":"台湾"},
-    {"n":"泰国","v":"泰国"},
-    {"n":"印度","v":"印度"},
-    {"n":"其他","v":"其他"}
-    ]
-    },		
-    {"key":"year","name":"时间","value":[
-    {"n":"全部时间","v":""},
-    {"n":"2024","v":"2024"},		
-    {"n":"2023","v":"2023"},
-    {"n":"2022","v":"2022"},
-    {"n":"2021","v":"2021"},
-    {"n":"2020","v":"2020"},
-    {"n":"10年代","v":"10年代"},
-    {"n":"00年代","v":"00年代"},
-    {"n":"90年代","v":"90年代"},
-    {"n":"80年代","v":"80年代"},
-    {"n":"更早","v":"更早"}
-    ]
-    },
-    {"key":"by","name":"排序","value":[
-    {"n":"全部排序","v":""},
-    {"n":"时间排序","v":"time"},
-    {"n":"人气排序","v":"hits"},
-    {"n":"评分排序","v":"score"}
-    ]
-    }
-    ],
-    "3":[	
-    {"key":"class","name":"剧情","value":[
-    {"n":"Netflix","v":"Netflix"},		
-    {"n":"全部剧情","v":""},
-    {"n":"动态漫画","v":"动态漫画"},
-    {"n":"剧情","v":"剧情"},
-    {"n":"动画","v":"动画"},
-    {"n":"喜剧","v":"喜剧"},
-    {"n":"冒险","v":"冒险"},
-    {"n":"动作","v":"动作"},
-    {"n":"奇幻","v":"奇幻"},
-    {"n":"科幻","v":"科幻"},
-    {"n":"儿童","v":"儿童"},
-    {"n":"搞笑","v":"搞笑"},
-    {"n":"爱情","v":"爱情"},
-    {"n":"家庭","v":"家庭"},
-    {"n":"短片","v":"短片"},
-    {"n":"热血","v":"热血"},
-    {"n":"益智","v":"益智"},
-    {"n":"悬疑","v":"悬疑"},
-    {"n":"经典","v":"经典"},
-    {"n":"校园","v":"校园"},
-    {"n":"Anime","v":"Anime"},
-    {"n":"运动","v":"运动"},
-    {"n":"亲子","v":"亲子"},
-    {"n":"青春","v":"青春"},
-    {"n":"恋爱","v":"恋爱"},
-    {"n":"武侠","v":"武侠"},
-    {"n":"惊悚","v":"惊悚"}
-    ]
-    },
-    {"key":"area","name":"地区","value":[
-    {"n":"全部地区","v":""},
-    {"n":"大陆","v":"大陆"},
-    {"n":"台湾","v":"台湾"},
-    {"n":"美国","v":"美国"},
-    {"n":"香港","v":"香港"},
-    {"n":"韩国","v":"韩国"},
-    {"n":"英国","v":"英国"},
-    {"n":"法国","v":"法国"},
-    {"n":"德国","v":"德国"},
-    {"n":"印度","v":"印度"},
-    {"n":"泰国","v":"泰国"},
-    {"n":"丹麦","v":"丹麦"},
-    {"n":"瑞典","v":"瑞典"},
-    {"n":"巴西","v":"巴西"},
-    {"n":"加拿大","v":"加拿大"},
-    {"n":"俄罗斯","v":"俄罗斯"},
-    {"n":"意大利","v":"意大利"},
-    {"n":"比利时","v":"比利时"},
-    {"n":"爱尔兰","v":"爱尔兰"},
-    {"n":"西班牙","v":"西班牙"},
-    {"n":"澳大利亚","v":"澳大利亚"},
-    {"n":"其他","v":"其他"}
-    ]
-    },		
-    {"key":"year","name":"时间","value":[
-    {"n":"全部时间","v":""},
-    {"n":"2024","v":"2024"},		
-    {"n":"2023","v":"2023"},
-    {"n":"2022","v":"2022"},
-    {"n":"2021","v":"2021"},
-    {"n":"2020","v":"2020"},
-    {"n":"10年代","v":"10年代"},
-    {"n":"00年代","v":"00年代"},
-    {"n":"90年代","v":"90年代"},
-    {"n":"80年代","v":"80年代"},
-    {"n":"更早","v":"更早"}
-    ]
-    },
-    {"key":"by","name":"排序","value":[
-    {"n":"全部排序","v":""},
-    {"n":"时间排序","v":"time"},
-    {"n":"人气排序","v":"hits"},
-    {"n":"评分排序","v":"score"}
-    ]
-    }
-    ],		
-    "4":[	
-    {"key":"class","name":"剧情","value":[
-    {"n":"全部剧情","v":""},
-    {"n":"纪录","v":"纪录"},
-    {"n":"真人秀","v":"真人秀"},
-    {"n":"记录","v":"记录"},
-    {"n":"脱口秀","v":"脱口秀"},
-    {"n":"剧情","v":"剧情"},
-    {"n":"历史","v":"历史"},
-    {"n":"喜剧","v":"喜剧"},
-    {"n":"传记","v":"传记"},
-    {"n":"相声","v":"相声"},
-    {"n":"节目","v":"节目"},
-    {"n":"歌舞","v":"歌舞"},
-    {"n":"冒险","v":"冒险"},
-    {"n":"运动","v":"运动"},
-    {"n":"Season","v":"Season"},
-    {"n":"犯罪","v":"犯罪"},
-    {"n":"短片","v":"短片"},
-    {"n":"搞笑","v":"搞笑"},
-    {"n":"晚会","v":"晚会"}
-    ]
-    },
-    {"key":"area","name":"地区","value":[
-    {"n":"全部地区","v":""},
-    {"n":"大陆","v":"大陆"},
-    {"n":"香港","v":"香港"},
-    {"n":"台湾","v":"台湾"},
-    {"n":"美国","v":"美国"},
-    {"n":"日本","v":"日本"},		
-    {"n":"韩国","v":"韩国"},
-    {"n":"其他","v":"其他"}
-    ]
-    },		
-    {"key":"year","name":"时间","value":[
-    {"n":"全部时间","v":""},
-    {"n":"2024","v":"2024"},		
-    {"n":"2023","v":"2023"},
-    {"n":"2022","v":"2022"},
-    {"n":"2021","v":"2021"},
-    {"n":"2020","v":"2020"},
-    {"n":"10年代","v":"10年代"},
-    {"n":"00年代","v":"00年代"},
-    {"n":"90年代","v":"90年代"},
-    {"n":"80年代","v":"80年代"},
-    {"n":"更早","v":"更早"}
-    ]
-    },
-    {"key":"by","name":"排序","value":[
-    {"n":"全部排序","v":""},
-    {"n":"时间排序","v":"time"},
-    {"n":"人气排序","v":"hits"},
-    {"n":"评分排序","v":"score"}
-    ]
-    }
-    ],
-    "6":[	
-    {"key":"class","name":"剧情","value":[
-    {"n":"全部剧情","v":""},
-    {"n":"逆袭","v":"逆袭"},
-    {"n":"甜宠","v":"甜宠"},
-    {"n":"虐恋","v":"虐恋"},
-    {"n":"穿越","v":"穿越"},
-    {"n":"重生","v":"重生"},
-    {"n":"剧情","v":"剧情"},
-    {"n":"科幻","v":"科幻"},
-    {"n":"武侠","v":"武侠"},
-    {"n":"爱情","v":"爱情"},
-    {"n":"动作","v":"动作"},
-    {"n":"战争","v":"战争"},
-    {"n":"冒险","v":"冒险"},
-    {"n":"其它","v":"其它"}
-    ]
-    },
-    {"key":"by","name":"排序","value":[
-    {"n":"全部排序","v":""},
-    {"n":"时间排序","v":"time"},
-    {"n":"人气排序","v":"hits"},
-    {"n":"评分排序","v":"score"}
-    ]
-    }
-    ]
-    },
+    filter: 'H4sIAAAAAAAAA+2Zz08bRxTH/xefOdgGtTi3HlqpUpVLe6hURREHV4qa0kN/qFWEZLANxhBsEDFx7AIpGEyCf0CQY9bY/md2Ztf/RWf95r0ZR+3LtqGRqviC+LzvzOzs7Nt531k/isQid755FPku+VvkTsS76In9jchMZHHh+6TNvyw8/Dk5briowiJbH6XrQVhBZGkGoneTP3378MGvOnz3068+++Lzr0kV66cyndWiBtJKVRVBDYC0fN3tV1EDQM3LXZgxNaAml4syVdKaBtLSebnyDDUAGjPf8vovcEwA0k63xXUPNQAac+XcK23jmAB0D7U1008Daas7o/IZagA0Zu6p66zjmADUb2tVFC6xHwBphWP/iNYagLRmRzgN1ABQc28O/WZbaxpoLo0Td3CIcwEw2qaf2ydtDLRmBw1vfQ3XDMB6tt5uzzzbAEjLDL2XNdQAUPOXN0XV0ZqGpXuBCmksqm2x6Zg0Jg6TxuL4dFRexUXoNkSlr0PYYnRSlt3WRAsdMg+gLa8Hk2NAiJZlsKWCuCwAtJx7NVk9x+UEoGsfnJl+GmhZNi6MpoHGfPXEaBpotoPXRtNA2uO2cE5QAzBjtu0x23Y/t3s9on4a6N6390W2i/cOQNd7feXXhng9AJMuh3JjqB4GZQwyXXWY8fp7skQPh5jmnCmoDiKHb5xhatHaVSj3OtiC2Np2RHtXZPFNMUxPojb0CirVy/gwiOkqg1dwXdehjcgO0T1nO24PtzENdqr7rYZfT5lUJw6V6pW+ao+DA9BNXh4bTYOVaEbTYCWv0TRYyWs0DVaCWv0Awi6CuL5ye33rfUcOswjxaHxOx8b/WvFZE5+143ETj9vxmInH7HjUxKMUj0XH0zwiLRa9r/4kqEH0zQbRoEHUNEhMNoglEtH76o9pMP9mg/mgwbxpICtXcg9fhUD7ODGxsHJrRzgFs7DEkwvr9W5EMYdXMaNXU7KEL0ncDntpfNJmXf1WRuRw350LpnFvRvX6b8yJpjDmhDMZnHHhjARrFrjizZghrrBzhoczJ0FhovvTEMYMceaEMzVBQaPraQhjQNSWZ56RhjBGkDNfo3RfdFdwLgC3bUAoHLllA8LZhH9rPTgLwVkP1l681ShxJoMzJ9PCOS2cH3DhnH1PhTNfl6llefNy4hxnQqFO/9wpkPsywGzebKHkCh5X1JgTqSzue+dU0AFCGQimaHMnZ5UH/vMUagCkVfKyjFVFQxjj4fUK1rEMgPodPhcV2mQAUPtk8YFKK5Dgf8rMYVGtPW53AHRUcy5Fo4g7PwBtd7/vyKe40hpMOd9Qa0jlfAxhLMJfWqDb+mbAlc23l/N3+1rwbmZg+s1g+s1gan1snlqf/7n1mbst6+M5L0T/CaYzAGnVA9dxvFOqwcQ0t2bb9NZAWuZCFI5Mb8OhDBP36Z8xTNznfa/SFUe0OQHQbPMrXqWJUwUwJffvP/1zBo2zBl8mF378YVFrGmie3M8zjGHiDJosP3NvaEcFmP6cMC0j0zLyIZeRj/55GTFh+ymPUqv+H5R6AJTqu1XRxBOLBppVuajON5jqANTvbOh38tgPgK639tjbPcDrAYQpK+yv28zJij3fMudw9tdt7myf7Yhm2rySAbz3zFEpsvQnK+G0IDwhAAA=',
     一级f: `js:
     let urls = [
-    'https://keke5.app/show/1-----1-1.html',
-    'https://keke5.app/show/2-----1-1.html',
-    'https://keke5.app/show/3-----1-1.html',
-    'https://keke5.app/show/4-----1-1.html',
-    'https://keke5.app/show/6-----1-1.html',
+    'https://www.keke1.app/show/1-----1-1.html',
+    'https://www.keke1.app/show/2-----1-1.html',
+    'https://www.keke1.app/show/3-----1-1.html',
+    'https://www.keke1.app/show/4-----1-1.html',
+    'https://www.keke1.app/show/6-----1-1.html',
     ];
     let filters = {};
     pdfa = jsp.pdfa;
