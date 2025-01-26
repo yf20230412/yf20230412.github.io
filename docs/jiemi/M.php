70a8dcd1342832d548252c7a03748fb8f5fb14a7
yf1688 <yufeng1688778@163.com>
2025-01-26 14:26:00 +0800

    日常更新

---
+++ docs/jiemi/M.php
@@ -0,0 +1,18 @@
+<?php
+header('Content-Type: application/json');
+
+// 指定要获取的JSON数据的URL
+$jsonUrl = 'https://cdn09022024.gitlink.org.cn/api/v1/repos/yf1688/box/raw/master/M.json'; // 替换为您的实际URL
+
+// 获取JSON数据
+$jsonData = file_get_contents($jsonUrl);
+
+// 检查获取数据是否成功
+if ($jsonData === FALSE) {
+    echo json_encode(['error' => '无法获取数据']);
+    exit;
+}
+
+// 输出获取到的JSON数据
+echo $jsonData;
+?>
