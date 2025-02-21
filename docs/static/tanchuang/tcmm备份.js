7b0c4db2c706219901784dfd6f1c73dd810f9179
yf1688 <yufeng1688778@163.com>
2025-02-21 13:32:42 +0800

    日常更新

--- docs/static/tanchuang/tcmm备份.js
+++ docs/static/tanchuang/tcmm备份.js
@@ -1,19 +1,20 @@
 swal({
-            title: '关注微信公众号',
-            text: "公众号∶【风言锋语88】",
-            type: 'input',
-            closeOnConfirm: false,
-            closeOnCancel: false,
-            confirmButtonText: "确 认",
-            imageUrl: 'https://yf1688.top/static/tanchuang/wxgzh.jpg', imageSize: '200x200',
-            inputPlaceholder: "关注微信公众号后台回复∶验证码",
-            showLoaderOnConfirm: true,
-        }, function(inputValue) {
-            if (inputValue != '2025') {
-                swal.showInputError('输入错误，关注公众号后台回复∶验证码');
-                return;
-            } else {
-                swal.close('xiaoyu');
-                return;
-            }
-        });
+    title: '关注微信公众号',
+    text: "公众号∶【风言锋语88】",
+    type: 'input',
+    closeOnConfirm: false,
+    closeOnCancel: false,
+    confirmButtonText: "确 认",
+    imageUrl: 'https://yf1688.top/static/img/wxgzh.jpg',
+    imageSize: '200x200',
+    inputPlaceholder: "关注微信公众号后台回复∶验证码",
+    showLoaderOnConfirm: true,
+}, function(inputValue) {
+    if (inputValue != '2025') {
+        swal.showInputError('输入错误，关注公众号后台回复∶验证码');
+        return;
+    } else {
+        swal.close('xiaoyu');
+        return;
+    }
+});
