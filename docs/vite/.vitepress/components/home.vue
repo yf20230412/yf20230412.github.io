fc03839c154ff3ed377add3f9f600bbbf3c54388
yf1688 <yufeng1688778@163.com>
2025-03-24 19:49:33 +0800

    博客更新

---
+++ docs/vite/.vitepress/components/home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="home-wrapper">
+    <div v-for="item in list" :key="item" class="home-item">{{ item }}</div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const list = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+</script>
+
+<style scoped>
+.home-wrapper {
+  display: flex;
+  justify-content: center;
+  margin-top: 40px;
+}
+.home-item {
+  vertical-align: middle;
+  margin: 4px 4px 10px;
+  padding: 4px 8px;
+  font-weight: bolder;
+  display: inline-block;
+  cursor: pointer;
+  border-radius: 2px;
+  line-height: 13px;
+  font-size: 13px;
+  box-shadow: 0 1px 8px 0 rgba(0, 0, 0, 0.1);
+  transition: all 0.5s;
+}
+</style>
---
+++ docs/vite/.vitepress/theme/MyLayout.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+import DefaultTheme from 'vitepress/theme';
+import { useData } from 'vitepress';
+const { Layout } = DefaultTheme;
+
+const { frontmatter } = useData(); //这里的frontmatter就是各个md文件中自己写在最上面的东西
+</script>
+
+<template>
+  <Layout>
+    <template #doc-before>
+      <span class="page-info">✍️{{ frontmatter.author }}</span>
+    </template>
+  </Layout>
+</template>
+
+<style>
+.page-info {
+  font-size: 13px;
+  color: #7f7f7f;
+  margin-right: 10px;
+}
+</style>
